0998cb5e1ae3674cbdbddd6ec8780cd0408b6455
LeoLChalot <larouchalotleo@gmail.com>
2023-04-20 08:43:48 +0200

    SCRIPT | Traitement des résultats

--- src/Components/Bestiaire/Bestiaire.jsx
+++ src/Components/Bestiaire/Bestiaire.jsx
@@ -1,21 +1,27 @@
 import { useState, useEffect } from "react";
 import "./Bestiaire.css";
 import axios from "axios";
+// import { V4 as uuidv4 } from 'uuid';
+
 
 export default function Home() {
 	let [pokeArray, setPokeArray] = useState([]);
 	let offset = 0;
-	let limit = 1281;
+	let limit = 20;
 
 	useEffect(() => {
-		async function displayPokemon() {
+		async function displayPokemon(offset, limit) {
 			try {
 				axios
 					.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
 					.then((res) => {
 						let data = res.data;
 						console.log(data);
-						
+						console.log(pokeArray);
+						let results = data.results;
+						results.map((pokemon) => {
+							setPokeArray(pokeArray => pokeArray + pokemon)
+						});
 						console.log(pokeArray)
 					})
 					.catch((e) => console.log(e));
@@ -30,6 +36,14 @@ export default function Home() {
 		<>
 			<div className="Bestiaire-container">
 				<h1>Bestiaire</h1>
+				<div className="Pokemons-container">
+				{/* {pokeArray.map((pokemon) => {
+					<div className="Pokemon-card">
+						<p>{ pokemon.name }</p>
+					</div>
+				})} */}
+				</div>
+				
 			</div>
 		</>
 	);
