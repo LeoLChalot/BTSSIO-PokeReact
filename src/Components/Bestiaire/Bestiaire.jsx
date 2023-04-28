2102602847dc97c0148429ab6139a6823806e40d
LeoLChalot <larouchalotleo@gmail.com>
2023-04-28 11:29:46 +0200

    SCRIPT | Gestion du tri de la collection

--- src/Components/Bestiaire/Bestiaire.jsx
+++ src/Components/Bestiaire/Bestiaire.jsx
@@ -1,43 +1,56 @@
 import { useState, useEffect } from "react";
 import "./Bestiaire.css";
-import axios from "axios";
-import {v4 as uuidv4} from 'uuid'
-import dark from '../../assets/cards/type-dark.png'
-import electric from '../../assets/cards/type-electric.png'
-import fairy from '../../assets/cards/type-fairy.png'
-import fighting from '../../assets/cards/type-fighting.png'
-import fire from '../../assets/cards/type-fire.png'
-import ghost from '../../assets/cards/type-ghost.png'
-import grass from '../../assets/cards/type-grass.png'
-import psychic from '../../assets/cards/type-psychic.png'
-import steel from '../../assets/cards/type-steel.png'
-import water from '../../assets/cards/type-water.png'
-import unknown from '../../assets/cards/type-unknown.png'
+import axios, { all } from "axios";
+import { v4 as uuidv4 } from "uuid";
+import dark from "../../assets/cards/type-dark.png";
+import electric from "../../assets/cards/type-electric.png";
+import fairy from "../../assets/cards/type-fairy.png";
+import fighting from "../../assets/cards/type-fighting.png";
+import fire from "../../assets/cards/type-fire.png";
+import ghost from "../../assets/cards/type-ghost.png";
+import grass from "../../assets/cards/type-grass.png";
+import psychic from "../../assets/cards/type-psychic.png";
+import steel from "../../assets/cards/type-steel.png";
+import water from "../../assets/cards/type-water.png";
+import unknown from "../../assets/cards/type-unknown.png";
 
 export default function Home() {
 	let [allPokemon, setAllPokemon] = useState([]);
-	let [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
-	let [urlPlus, setUrlPlus] = useState('');
-	let [urlMoins, setUrlMoins] = useState('');
-	let urlLast = "https://pokeapi.co/api/v2/pokemon?offset=1261&limit=1"
+	let [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20");
+	let [urlPlus, setUrlPlus] = useState("");
+	let [urlMoins, setUrlMoins] = useState("");
+	let urlLast = "https://pokeapi.co/api/v2/pokemon?offset=1261&limit=1";
 	let pokedex = [];
 
-	useEffect(() => {displayPokemon(url)}, []);
+	useEffect(() => {
+		displayPokemon(url);
+	}, []);
 
-	async function displayPokemon(url) {
+	async function displayPokemon(url, sort) {
+		
 		try {
 			// ? On récupère la liste de l'ensemble des pokémons (limit et offset à utiliser pour la pagination)
 			axios
 				.get(url)
 				.then((res) => {
-					console.log(res)
+					console.log(res);
 					const data = res.data;
 					const results = data.results;
-					setUrlPlus(urlMoins => data.next)
+					console.log(results);
+					if (sort != undefined) {
+						if(sort == "alpha")
+						results.sort(function compare(a, b) {
+							if (a.name < b.name) return -1;
+							if (a.name > b.name) return 1;
+							return 0;
+						});
+					}
+
+					setUrlPlus((urlMoins) => data.next);
 					// urlPlus = data.next;
-					setUrlMoins(urlPlus => data.previous)
+					setUrlMoins((urlPlus) => data.previous);
 					// urlMoins = data.previous;
-					console.log(urlPlus, urlMoins)
+					console.log(urlPlus, urlMoins);
 
 					// ? Pour chaque résultat, on récupère les informations (Object) pour chaque pokémon
 					results.map(async (pokemon) => {
@@ -50,6 +63,7 @@ export default function Home() {
 							(e) => console.log(e);
 						}
 					});
+					// console.log(allPokemon)
 				})
 
 				.catch((e) => console.log(e));
@@ -58,45 +72,58 @@ export default function Home() {
 		}
 	}
 
-	function pageFirst(){
-		setAllPokemon(allPokemon => [])
+	function pageFirst() {
+		setAllPokemon((allPokemon) => []);
 		displayPokemon(url);
 	}
-	function pageLast(){
-		setAllPokemon(allPokemon => [])
+	function pageLast() {
+		setAllPokemon((allPokemon) => []);
 		displayPokemon(urlLast);
 	}
-	function pagePlus(){
-		setAllPokemon(allPokemon => [])
+	function pagePlus() {
+		setAllPokemon((allPokemon) => []);
 		displayPokemon(urlPlus);
 	}
-	function pageMoins(){
-		setAllPokemon(allPokemon => [])
-		displayPokemon(urlMoins);
+	function pageMoins() {
+		setAllPokemon((allPokemon) => []);
+		if (urlMoins != undefined) {
+			displayPokemon(urlMoins);
+		}
 	}
+	function addToPokedex(pokemon) {}
 
-	function addToPokedex(pokemon){
-
-	}
+	// function sortAZ(){
+	// 	setAllPokemon(allPokemon.sort())
+	// 	console.log(allPokemon)
+	// }
 
 	// console.log(allPokemon)
 
 	return (
 		<>
 			<main>
+				{/* <div className="btn-container-sort">
+					<button onClick={displayPokemon(url)}>All</button>
+					<button onClick={() => {
+						setAllPokemon((allPokemon) => []);
+						displayPokemon("alpha")}}>A-Z</button>
+					<button>Type</button>
+				</div> */}
+
 				<div className="pokemon-container">
 					{allPokemon.map((pokemon) => {
 						return (
-							<>	
-							
-							<div key={uuidv4()}className="poke-card" type={pokemon.types[0].type.name}>
+							<>
+								<div key={uuidv4()} className="poke-card" type={pokemon.types[0].type.name}>
 									{console.log(pokemon)}
-									
+
 									<img src={pokemon.sprites.front_default} alt="image du pokémon" />
 									<p>{pokemon.name}</p>
 									<div className="btn-container">
 										<button className="btn-info">Info</button>
-										<button className="btn-add" onClick={addToPokedex(pokemon.name)}>Add</button>
+										<button className="btn-add" onClick={addToPokedex(pokemon.name)}>
+											Add
+										</button>
 									</div>
 								</div>
 							</>
@@ -104,13 +131,20 @@ export default function Home() {
 					})}
 				</div>
 				<div className="pagination">
-					<button id="pagination-moins" onClick={pageMoins}>⬅️</button>
-					<button id="pagination-first" onClick={pageFirst}>⏮️</button>
-					<button id="pagination-last" onClick={pageLast}>⏭️</button>
-					<button id="pagination-plus" onClick={pagePlus}>➡️</button>
+					<button id="pagination-moins" onClick={pageMoins}>
+						⬅️
+					</button>
+					<button id="pagination-first" onClick={pageFirst}>
+						⏮️
+					</button>
+					<button id="pagination-last" onClick={pageLast}>
+						⏭️
+					</button>
+					<button id="pagination-plus" onClick={pagePlus}>
+						➡️
+					</button>
 				</div>
 			</main>
-
 		</>
 	);
 }
